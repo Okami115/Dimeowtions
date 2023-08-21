72d51b1b9ce5dafb46ebdef5d1ff01d2691b3f6a
Okami <tomaspp515@gmail.com>
2023-08-21 18:18:40 -0300

    Add Noir Shader and particles

---
+++ Assets/Scripts/Terrain/SpawnObstacles.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawners;
+
+
+
+}
