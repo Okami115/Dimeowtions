628b0e8c7dec44c27faf6faf729630ba61bdae9b
matiaspierpaoli <matiaspierpaoli@gmail.com>
2023-09-02 14:18:37 -0300

    Move menu canvas to a new scene called Main Menu

--- Assets/Scripts/Scene Managment/SceneManager.cs
+++ Assets/Scripts/Scene Managment/SceneManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
+    //[SerializeField] private string sceneName;
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void ExitGame()
     {
         Application.Quit();
