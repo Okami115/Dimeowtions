c02f56156948640162abeecaf9136644ea072f87
matiaspierpaoli <matiaspierpaoli@gmail.com>
2023-09-21 17:24:35 -0300

    Implement noir walk animation

--- Assets/Scripts/Game Management/AnimationTrigger.cs
+++ Assets/Scripts/Game Management/AnimationTrigger.cs
@@ -35,7 +35,5 @@ public class AnimationTrigger : MonoBehaviour
         animator.speed = (interactionAnimationDuration / playerStats.initialSpeed) / speedFactor;
 
         animator.Play(interactionAnimationName);
-
-        //animator.speed = animatorDefaultSpeed;
     }
 }
