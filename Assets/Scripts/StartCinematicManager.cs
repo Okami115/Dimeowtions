4709b5529af3e4b2f851082bdee8f93b754b8623
matiaspierpaoli <matiaspierpaoli@gmail.com>
2023-11-17 17:55:02 -0300

    Change first cinematic skip to hold input

--- Assets/Scripts/StartCinematicManager.cs
+++ Assets/Scripts/StartCinematicManager.cs
@@ -7,13 +7,39 @@ public class StartCinematicManager : MonoBehaviour
     [SerializeField] private VideoPlayer video;
     [SerializeField] private SceneLoader sceneLoader;
     [SerializeField] private int mainMenuLevelindex;
+    [SerializeField] private float holdDuration = 1.0f;
+
+    private bool isHoldingKey = false;
+    private float holdTimer = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (video.isPaused || Keyboard.current.anyKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame)
+        if (isHoldingKey)
         {
-            sceneLoader.LoadLevel(mainMenuLevelindex);
+            holdTimer += Time.deltaTime;
+
+            if (holdTimer >= holdDuration)
+            {
+                LoadNextScene();
+            }
+
+            if (!Keyboard.current.anyKey.isPressed)
+            {
+                isHoldingKey = false;
+                holdTimer = 0.0f;
+            }
         }
+    }
+
+    public void OnSkipCinematic()
+    {
+        isHoldingKey = true;
+        holdTimer = 0.0f;
+    }
+
+    private void LoadNextScene()
+    {
+        sceneLoader.LoadLevel(mainMenuLevelindex);
     }
 }
