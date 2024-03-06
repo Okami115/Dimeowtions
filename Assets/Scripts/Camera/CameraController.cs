fc26a886c4208ef7a8c1031cbbfce302d4a0f680
OkamiDesu <tomaspp515@gmail.com>
2024-03-06 04:05:48 -0300

    Add FOV in Movements

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,28 +8,37 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private PlayerController playerController;
     [SerializeField] private Camera camera;
-
+    [SerializeField] private float JumpMultiplier;
+    [SerializeField] private float MoveMultiplier;
 
     private void OnEnable()
     {
         playerController.jump += ReduceFOV;
+        playerController.moveAction += IncreaseFOV;
     }
 
     private void OnDisable()
     {
         playerController.jump -= ReduceFOV;
+        playerController.moveAction -= IncreaseFOV;
     }
 
     void Update()
     {
         if(camera.fieldOfView > 120)
-        {
-            camera.fieldOfView--;
-        }
+            camera.fieldOfView -= Time.deltaTime * JumpMultiplier;
+
+        if (camera.fieldOfView < 120)
+            camera.fieldOfView += Time.deltaTime * MoveMultiplier;
     }
 
     private void ReduceFOV()
     {
         camera.fieldOfView = 150;
+    }
+
+    private void IncreaseFOV()
+    {
+        camera.fieldOfView = 90;
     }
 }
